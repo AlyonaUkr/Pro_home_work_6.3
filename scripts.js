af3c9c3299f2884fbd2c58279d4d5d7e4ed4724e
AlyonaUkr <mochulska1994@gmail.com>
2022-12-18 19:34:25 +0200

    ready

---
+++ scripts.js
@@ -0,0 +1,11 @@
+let userResult = prompt('Input number or string');
+
+switch (userResult) {
+    case 'string':
+        alert('You choose string');
+    case 'number':
+        alert('You choose number');
+    default:
+        alert('You choose nothing');
+}
+
